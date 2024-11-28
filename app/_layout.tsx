e4645730b906364f0d4252c4ab241c5a1b91c761
Xnths <jonathascastilhoes@gmail.com>
2024-11-28 16:27:46 -0300

    feat: adding navigation and pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,47 @@
+import { primaryColor } from "@/lib/colors";
+import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Tabs/>
+
+  const iconSize = 24;
+
+  return <Tabs screenOptions={{tabBarActiveTintColor: primaryColor}}>
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: "Mapa",
+        tabBarIcon: ({color}) => <FontAwesome size={iconSize} name="home" color={color} />
+      }}
+    />
+    <Tabs.Screen
+      name="parques"
+      options={{
+        title: "Parques",
+        tabBarIcon: ({color}) => <FontAwesome size={iconSize} name="tree" color={color} />
+      }}
+    />
+    <Tabs.Screen
+      name="agenda"
+      options={{
+        title: "Agenda",
+        tabBarIcon: ({color}) => <FontAwesome size={iconSize} name="calendar" color={color} />
+      }}
+    />
+    <Tabs.Screen
+      name="chat"
+      options={{
+        title: "Chat",
+        tabBarIcon: ({color}) => <FontAwesome size={iconSize} name="comment" color={color} />
+      }}
+    />
+    <Tabs.Screen
+      name="favoritos"
+      options={{
+        title: "Favoritos",
+        tabBarIcon: ({color}) => <FontAwesome size={iconSize} name="heart" color={color} />
+      }}
+    />
+  </Tabs>
 }
---
+++ lib/colors.tsx
@@ -0,0 +1,1 @@
+export const primaryColor = "#92CC00"
