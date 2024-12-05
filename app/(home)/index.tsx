b6ec3084a62d26f142b40d2192c5e5763193cdc4
Xnths <jonathascastilhoes@gmail.com>
2024-12-05 13:13:40 -0300

    feat: adding filter

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,13 +1,47 @@
 import { primaryColor } from "@/lib/colors";
 import { FontAwesome } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { StyleSheet, View, Image } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-const soccerImage = require("@/assets/images/soccer.png")
-const basketImage = require("@/assets/images/basket.png")
+const soccerImage = require("@/assets/images/soccer.png");
+const basketImage = require("@/assets/images/basket.png");
+
+const soccerMarkers = [
+  {
+    id: 1,
+    coordinate: { latitude: -23.5458649, longitude: -46.7287297 },
+    title: "Av. Prof. Fonseca Rodrigues, 2001",
+    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+  },
+  {
+    id: 3,
+    coordinate: { latitude: -23.5462345, longitude: -46.7271344 },
+    title: "Av. Prof. Fonseca Rodrigues, 2001",
+    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+  }
+];
+
+const basketMarkers = [
+  {
+    id: 2,
+    coordinate: { latitude: -23.545312, longitude: -46.727184 },
+    title: "Av. Prof. Fonseca Rodrigues, 2001",
+    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+  },
+  {
+    id: 4,
+    coordinate: { latitude: -23.5462338, longitude: -46.7271339 },
+    title: "Av. Prof. Fonseca Rodrigues, 2001",
+    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+  }
+];
 
 export default function Index() {
+  const [showSoccer, setShowSoccer] = useState(true);
+  const [showBasket, setShowBasket] = useState(true);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -30,46 +64,35 @@ export default function Index() {
           }
         ]}
       >
-        <Marker
-          coordinate={{
-            latitude: -23.5458649,
-            longitude: -46.7287297,
-          }}
-          icon={soccerImage}
-          title="Av. Prof. Fonseca Rodrigues, 2001"
-          description="Alto de Pinheiros, São Paulo - SP, 05461-010"
-        />
-        <Marker
-          coordinate={{
-            latitude: -23.545312,
-            longitude: -46.727184,
-          }}
-          icon={basketImage}
-          title="Av. Prof. Fonseca Rodrigues, 2001"
-          description="Alto de Pinheiros, São Paulo - SP, 05461-010"
-        />
-        <Marker
-          coordinate={{
-            latitude: -23.5462345,
-            longitude: -46.7271344,
-          }}
-          icon={soccerImage}
-          title="Av. Prof. Fonseca Rodrigues, 2001"
-          description="Alto de Pinheiros, São Paulo - SP, 05461-010"
-        />
-        <Marker
-          coordinate={{
-            latitude: -23.5462338,
-            longitude: -46.7271339,
-          }}
-          icon={basketImage}
-          title="Av. Prof. Fonseca Rodrigues, 2001"
-          description="Alto de Pinheiros, São Paulo - SP, 05461-010"
-        />
+        {showSoccer && soccerMarkers.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={marker.coordinate}
+            icon={soccerImage}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+        
+        {showBasket && basketMarkers.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={marker.coordinate}
+            icon={basketImage}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
       </MapView>
-      <Link href="/ar" style={styles.arButton}>
-        <FontAwesome size={32} name="camera" />
-      </Link>
+      <View style={styles.bottomOptions}>
+        <View style={{display: "flex", flexDirection: "row", gap: 4, alignItems: "center"}}>
+          <Button onPress={() => setShowBasket(!showBasket)} title="Futebol" />
+          <Button onPress={() => setShowSoccer(!showSoccer)} title="Basquete" />
+        </View>
+        <Link href="/ar" style={styles.arButton}>
+          <FontAwesome size={32} name="camera" />
+        </Link>
+      </View>
     </View>
   );
 }
@@ -82,10 +105,16 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%"
   },
-  arButton: {
+  bottomOptions: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
     position: "absolute",
     bottom: 20,
-    right: 20,
+    left: 20,
+  },
+  arButton: {
     backgroundColor: primaryColor,
     padding: 10,
     borderRadius: 20,
