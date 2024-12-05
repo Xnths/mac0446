9dce5f3e140605712f699054d460565cd7045c8d
Xnths <jonathascastilhoes@gmail.com>
2024-12-05 16:27:38 -0300

    feat: adding filter buttons

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -2,7 +2,7 @@ import { primaryColor } from "@/lib/colors";
 import { FontAwesome } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { useState } from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 const soccerImage = require("@/assets/images/soccer.png");
@@ -12,14 +12,14 @@ const soccerMarkers = [
   {
     id: 1,
     coordinate: { latitude: -23.5458649, longitude: -46.7287297 },
-    title: "Av. Prof. Fonseca Rodrigues, 2001",
-    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+    title: "Quadra do auditório",
+    description: "Quadra poliesportiva."
   },
   {
     id: 3,
     coordinate: { latitude: -23.5462345, longitude: -46.7271344 },
-    title: "Av. Prof. Fonseca Rodrigues, 2001",
-    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+    title: "Quadra Powerade",
+    description: "Campo de futebol aberto."
   }
 ];
 
@@ -27,14 +27,14 @@ const basketMarkers = [
   {
     id: 2,
     coordinate: { latitude: -23.545312, longitude: -46.727184 },
-    title: "Av. Prof. Fonseca Rodrigues, 2001",
-    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+    title: "Quadra esporte e lazer",
+    description: "Meia quadra."
   },
   {
     id: 4,
     coordinate: { latitude: -23.5462338, longitude: -46.7271339 },
-    title: "Av. Prof. Fonseca Rodrigues, 2001",
-    description: "Alto de Pinheiros, São Paulo - SP, 05461-010"
+    title: "Quadra infantil",
+    description: "Quadra reduzida para prática de esporte infantil."
   }
 ];
 
@@ -84,10 +84,23 @@ export default function Index() {
           />
         ))}
       </MapView>
+      
       <View style={styles.bottomOptions}>
-        <View style={{display: "flex", flexDirection: "row", gap: 4, alignItems: "center"}}>
-          <Button onPress={() => setShowBasket(!showBasket)} title="Futebol" />
-          <Button onPress={() => setShowSoccer(!showSoccer)} title="Basquete" />
+        <View style={{ display: "flex", flexDirection: "row", gap: 4, alignItems: "center" }}>
+          <Pressable
+            onPress={() => setShowBasket(!showBasket)}
+            style={[styles.button, showBasket ? styles.activeFilter : styles.disabledFilter]}
+          >
+            {showBasket && <FontAwesome size={16} name="remove" color="white" />}
+            <Text style={styles.buttonText}>Futebol</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => setShowSoccer(!showSoccer)}
+            style={[styles.button, showSoccer ? styles.activeFilter : styles.disabledFilter]}
+          >
+            {showSoccer && <FontAwesome size={16} name="remove" color="white" />}
+            <Text style={styles.buttonText}>Basquete</Text>
+          </Pressable>
         </View>
         <Link href="/ar" style={styles.arButton}>
           <FontAwesome size={32} name="camera" />
@@ -120,5 +133,27 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     color: "white",
     textAlign: "center"
+  },
+  button: {
+    padding: 10,
+    borderRadius: 20,
+    width: 120,
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "row",
+    gap: 4,
+    fontWeight: "bold"
+  },
+  activeFilter: {
+    backgroundColor: primaryColor,
+  },
+  disabledFilter: {
+    backgroundColor: "#171717",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold"
   }
 });
